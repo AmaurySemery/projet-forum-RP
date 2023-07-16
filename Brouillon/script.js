208ef1bc216adea21b45897ad29fa43b9b083465
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2023-07-16 03:59:55 +0200

    avatars dans cellule

--- Brouillon/script.js
+++ Brouillon/script.js
@@ -2,3 +2,27 @@ function showAdditionalImage(image) {
     var additionalImage = document.getElementById("additionalImage");
     additionalImage.style.backgroundImage = "url(" + image.src + ")";
   }
+
+  // Récupérer l'élément avec l'ID "image-cercle"
+var imagePresentation = document.getElementById("image-presentation");
+
+// Récupérer l'élément avec la classe ".cellule-presentation"
+var cellulePresentation = document.querySelector(".cellule-presentation");
+
+// Récupérer l'URL de l'image à partir de l'attribut "data-image-url"
+var imageUrl = cellulePresentation.getAttribute("data-image-url");
+
+// Appliquer l'URL de l'image à l'attribut "src" de l'élément image
+imagePresentation.src = imageUrl;
+
+// Récupérer l'élément avec l'ID "image-cercle"
+var imagePouvoir = document.getElementById("image-pouvoir");
+
+// Récupérer l'élément avec la classe ".cellule-presentation"
+var cellulePresentation = document.querySelector(".cellule-combat");
+
+// Récupérer l'URL de l'image à partir de l'attribut "data-image-url"
+var imageUrl = cellulePresentation.getAttribute("data-image-url");
+
+// Appliquer l'URL de l'image à l'attribut "src" de l'élément image
+imagePouvoir.src = imageUrl;
