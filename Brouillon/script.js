594ef870befa9154ad371e3321de7a7c1bc5d93b
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2023-07-16 15:30:26 +0200

    Update script.js

--- Brouillon/script.js
+++ Brouillon/script.js
@@ -3,12 +3,12 @@ function showAdditionalImage(image) {
     additionalImage.style.backgroundImage = "url(" + image.src + ")";
   }
 
-  function afficherImage(elementImageId, celluleSelector) {
-    var imageElement = document.getElementById(elementImageId);
-    var celluleElement = document.querySelector(celluleSelector);
-    var imageUrl = celluleElement.getAttribute("data-image-url");
-    imageElement.src = imageUrl;
-  }
-  
-  afficherImage("image-presentation", ".cellule-presentation");
-  afficherImage("image-pouvoir", ".cellule-combat");
+function afficherImage(elementImageId, celluleSelector) {
+  var imageElement = document.getElementById(elementImageId);
+  var celluleElement = document.querySelector(celluleSelector);
+  var imageUrl = celluleElement.getAttribute("data-image-url");
+  imageElement.src = imageUrl;
+}
+
+afficherImage("image-presentation", ".cellule-presentation");
+afficherImage("image-pouvoir", ".cellule-combat");
