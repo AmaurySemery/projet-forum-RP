db8a22a8940670e0f65b501f73361122a664f20b
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2023-07-16 15:30:10 +0200

    Update script.js

--- Brouillon/script.js
+++ Brouillon/script.js
@@ -3,26 +3,12 @@ function showAdditionalImage(image) {
     additionalImage.style.backgroundImage = "url(" + image.src + ")";
   }
 
-  // Récupérer l'élément avec l'ID "image-cercle"
-var imagePresentation = document.getElementById("image-presentation");
-
-// Récupérer l'élément avec la classe ".cellule-presentation"
-var cellulePresentation = document.querySelector(".cellule-presentation");
-
-// Récupérer l'URL de l'image à partir de l'attribut "data-image-url"
-var imageUrl = cellulePresentation.getAttribute("data-image-url");
-
-// Appliquer l'URL de l'image à l'attribut "src" de l'élément image
-imagePresentation.src = imageUrl;
-
-// Récupérer l'élément avec l'ID "image-cercle"
-var imagePouvoir = document.getElementById("image-pouvoir");
-
-// Récupérer l'élément avec la classe ".cellule-presentation"
-var cellulePresentation = document.querySelector(".cellule-combat");
-
-// Récupérer l'URL de l'image à partir de l'attribut "data-image-url"
-var imageUrl = cellulePresentation.getAttribute("data-image-url");
-
-// Appliquer l'URL de l'image à l'attribut "src" de l'élément image
-imagePouvoir.src = imageUrl;
+  function afficherImage(elementImageId, celluleSelector) {
+    var imageElement = document.getElementById(elementImageId);
+    var celluleElement = document.querySelector(celluleSelector);
+    var imageUrl = celluleElement.getAttribute("data-image-url");
+    imageElement.src = imageUrl;
+  }
+  
+  afficherImage("image-presentation", ".cellule-presentation");
+  afficherImage("image-pouvoir", ".cellule-combat");
